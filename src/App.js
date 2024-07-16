f93888152c8d9b9baae25329dc8ca450240ca56c
sparkjames <jpfleiderer@gmail.com>
2024-07-16 13:36:18 -0400

    carrying over code from other project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="the-void graphic--windows">
+
+		<div class="starpoint">
+			{ /* Stars populate here. */ }
+		</div>
+
+		<div class="controls">
+			<h1 class="controls-title">Screensaver Setup</h1>
+
+			<div class="controls-group controls-group--graphic">
+				<h2 class="controls-heading">Graphic</h2>
+
+				<ul class="controls-list">
+					<li class="controls-list-item">
+						<label for="graphic-windows"><input type="radio" class="controls-radio controls-radio--graphic" name="graphic" id="graphic-windows" value="windows" checked />Windows</label>
+					</li>
+					<li class="controls-list-item">
+						<label for="graphic-stars"><input type="radio" class="controls-radio controls-radio--graphic" name="graphic" id="graphic-stars" value="stars" />Stars</label>
+					</li>
+				</ul>
+				
+			</div>
+
+			<div class="controls-group controls-group--quantity">
+				<h2 class="controls-heading">Warp Speed</h2>
+
+				<div class="controls-slide-container">
+					<div class="controls-slide-labels">
+						<p class="controls-slide-label">Slow</p>
+						<p class="controls-slide-label">Fast</p>
+					</div>
+					<input type="range" min="10" max="150" value="50" class="controls-slide controls-slide--quantity" id="controls-slide--quantity"/>
+				</div>
+			</div>
+			
+		</div>
+
+	</div>
   );
 }
 
