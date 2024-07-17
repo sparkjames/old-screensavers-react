f0ab8677cfedc8e1662f6f4efea062c0c3ad80c8
sparkjames <jpfleiderer@gmail.com>
2024-07-16 21:08:34 -0400

    windows svg appearing

--- src/components/star/star.styles.jsx
+++ src/components/star/star.styles.jsx
@@ -1,12 +1,19 @@
 import styled, { css } from "styled-components";
+import { getRandom16Color } from '../../utilities/getRandom16Color';
 
 export const StarEl = styled.div`
 	position: absolute;
 	left: 0;
 	top: 0;
+	height: 86px;
+	width: 71px;
 	left: ${props => props.$left || '0'};
 	top: ${props => props.$top || '0'};
 	transform: ${props => props.$transform || 'translate3d(0px, 0px, 0px)'};
 	opacity: ${props => props.$opacity || '0'};
 	transition: ${props => props.$transition || 'none'};
+
+	path {
+		fill: ${props => props.$color || '#f0f'};
+	}
 `;
