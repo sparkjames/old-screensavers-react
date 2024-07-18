4160cc7ff24a373667563b29f8cd783c7094b135
sparkjames <jpfleiderer@gmail.com>
2024-07-18 10:24:00 -0400

    component for warp speed controls

--- src/components/control-group-warpspeed/control-group-warpspeed.component.jsx
+++ src/components/control-group-warpspeed/control-group-warpspeed.component.jsx
@@ -1,6 +1,30 @@
 import './control-group-warpspeed.styles.scss';
 
+import { useContext } from 'react';
+import { WarpSpeedContext } from '../../contexts/warpSpeed.context';
+
 export const ControlGroupWarpSpeed = () => {
+
+	const { setWarpSpeed } = useContext(WarpSpeedContext);
+
+	const warpSpeedOnChange = (e) => {
+		setWarpSpeed( parseInt( e.target.value ) );
+		console.log('setting warpSpeed value to ', parseInt( e.target.value ));
+	};
+
+	return (
+		<div className="controls-group controls-group--quantity">
+			<h2 className="controls-heading">Warp Speed</h2>
+
+			<div className="controls-slide-container">
+				<div className="controls-slide-labels">
+					<p className="controls-slide-label">Slow</p>
+					<p className="controls-slide-label">Fast</p>
+				</div>
+				<input type="range" min="10" max="150" defaultValue="70" className="controls-slide controls-slide--quantity" id="controls-slide--quantity" onMouseUp={warpSpeedOnChange} />
+			</div>
+		</div>
+	);
 
 };
 
--- src/components/controls/controls.component.jsx
+++ src/components/controls/controls.component.jsx
@@ -1,16 +1,10 @@
 import './controls.styles.scss';
-import { useContext } from 'react';
-import { WarpSpeedContext } from '../../contexts/warpSpeed.context';
 import { ControlGroupGraphic } from '../control-group-graphic/control-group-graphic.component';
+import ControlGroupWarpSpeed from '../control-group-warpspeed/control-group-warpspeed.component';
 
 export const Controls = () => {
 
-	const { setWarpSpeed } = useContext(WarpSpeedContext);
-
-	const warpSpeedOnChange = (e) => {
-		setWarpSpeed( parseInt( e.target.value ) );
-		console.log('setting warpSpeed value to ', parseInt( e.target.value ));
-	};
+	
 
 	return (
 		<div className="controls">
@@ -18,17 +12,7 @@ export const Controls = () => {
 
         <ControlGroupGraphic></ControlGroupGraphic>
 
-        <div className="controls-group controls-group--quantity">
-          <h2 className="controls-heading">Warp Speed</h2>
-
-          <div className="controls-slide-container">
-            <div className="controls-slide-labels">
-              <p className="controls-slide-label">Slow</p>
-              <p className="controls-slide-label">Fast</p>
-            </div>
-            <input type="range" min="10" max="150" defaultValue="70" className="controls-slide controls-slide--quantity" id="controls-slide--quantity" onMouseUp={warpSpeedOnChange} />
-          </div>
-        </div>
+        <ControlGroupWarpSpeed></ControlGroupWarpSpeed>
         
       </div>
 	);
