9a114c7da275f946552b225f668c0feff9f7d6d6
sparkjames <jpfleiderer@gmail.com>
2024-07-28 13:26:30 -0400

    add opacity change to controls

--- src/components/controls/controls.component.jsx
+++ src/components/controls/controls.component.jsx
@@ -2,18 +2,34 @@ import './controls.styles.scss';
 import { ControlGroupGraphic } from '../control-group-graphic/control-group-graphic.component';
 import ControlGroupWarpSpeed from '../control-group-warpspeed/control-group-warpspeed.component';
 import ControlGroupQuantity from '../control-group-quantity/control-group-quantity.component';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export const Controls = () => {
 
 	const [hideControls, setHideControls] = useState(false);
+	const controlsRef = useRef(null);
 
 	const titleBarCloseOnClick = () => {
 		setHideControls( !hideControls );
 	};
 
+	const controlsOnMouseOver = () => {
+		console.log(controlsRef.current);
+		controlsRef.current.style.opacity = '1.0';
+	};
+	const controlsOnMouseLeave = () => {
+		console.log(controlsRef.current);
+		controlsRef.current.style.opacity = '0.33';
+	};
+
 	return (
-		<div className="controls">
+		<div 
+			className="controls" 
+			ref={controlsRef} 
+			onMouseOver={controlsOnMouseOver}
+			onMouseLeave={controlsOnMouseLeave}
+			style={{opacity: '0.33'}}
+			>
 			<div className="controls-titleBar">
 				<button className="controls-titleBarClose" type="button" onClick={titleBarCloseOnClick}>Toggle Controls<span className="controls-titleBarCloseBar"></span></button>
 				<h1 className="controls-title">Screensaver Setup</h1>
